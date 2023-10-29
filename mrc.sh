894ad16357d086ad97d69b464932766eb12009d8
ChrisCoder9000 <alch.infoemail@gmail.com>
2023-10-29 13:22:45 +0100

    added help fn

--- mrc.sh
+++ mrc.sh
@@ -2,17 +2,6 @@
 
 SCRIPT_DIR="/Users/christiannonis/Documents/mrc"
 
-if [[ "$1" == "--help" ]]; then
-    echo "Usage: mrc <component-name> [--page --atom --sc=<style-component>]"
-    echo ""
-    echo "Options:"
-    echo "  --page                Create a page component"
-    echo "  --atom                Create an atom component"
-    echo "  --sc=<style-component>   Specify the style component for the atom component"
-    exit 0
-fi
-
-
 if [ ! -f "package.json" ]; then
     echo "Warning: 'package.json' not found. Make sure you are in a React project."
     exit 1  # Exit script if package.json is not found
@@ -32,9 +21,13 @@ IS_PAGE=false
 IS_ATOM=false
 STYLE_COMPONENT="div"
 TEMPLATE_DIR="$SCRIPT_DIR/templates"
+HELP=false
 
 for arg in "$@"; do
     case $arg in
+        --help)
+            HELP=true  # Imposta HELP a true se --help è specificato
+        ;;
         --page)
             IS_PAGE=true
         ;;
@@ -53,12 +46,6 @@ for arg in "$@"; do
     esac
 done
 
-if [[ -z $COMPONENT_NAME ]]; then
-    echo "Usage: mrc <component-name> [--page --atom --sc=<style-component>]"
-    exit 1
-fi
-
-
 if $IS_PAGE; then
     create_page $COMPONENT_NAME
 fi
@@ -67,6 +54,21 @@ if $IS_ATOM; then
     create_atom $COMPONENT_NAME
 fi
 
-if ! $IS_PAGE && ! $IS_ATOM; then
+if ! $IS_PAGE && ! $IS_ATOM && ! $HELP; then
     create_component $COMPONENT_NAME
+fi
+
+if $HELP; then
+    echo "Usage: mrc <component-name> [--page --atom --sc=<style-component>]"
+    echo ""
+    echo "Options:"
+    echo "  --page                Create a page component"
+    echo "  --atom                Create an atom component"
+    echo "  --sc=<style-component>   Specify the style component for the atom component"
+    exit 0
+fi
+
+if [[ -z $COMPONENT_NAME ]]; then
+    echo "Usage: mrc <component-name> [--page --atom --sc=<style-component>]"
+    exit 1
 fi
